05d1cfd78abf6b11aba42fefc2bde32c2c4457a8
charlesmagutu11 <charlesmagutu11@gmail.com>
2024-12-15 18:13:20 +0300

    added default values to triggerdby and status

--- backend/src/main/java/com/condabu/qadash/entity/TestRun.java
+++ backend/src/main/java/com/condabu/qadash/entity/TestRun.java
@@ -3,6 +3,7 @@ package com.condabu.qadash.entity;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,10 +24,12 @@ public class TestRun {
     private Long Id;
     private String runId;
     private LocalDate runDate ;
-    private String status;
+   // @Column(nullable = false,columnDefinition = "varchar(100) default 'RUNNING'")
+    private String status ="RUNNING";
     private String duration;
     private String suite;
-    private String triggeredBy;
+   // @Column(nullable = false,columnDefinition = "varchar(200) default 'CICD'")
+    private String triggeredBy = "CICD";
     private Long application;
     private Integer total;
     private Integer passed;
